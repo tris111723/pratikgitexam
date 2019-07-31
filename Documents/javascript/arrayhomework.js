1265676fa2d660bc75b69dd7721f4aa06b50bc0f
Azor Foucorlt <azorfoucorlt36@gmail.com>
2019-07-31 12:25:16 -0700

    DEVOIR MAP FUNCTION

---
+++ Documents/javascript/arrayhomework.js
@@ -0,0 +1,25 @@
+let rasinkare = [1,2,3,4,5]
+const map1 = rasinkare.map(x=> x*x)
+console.log(map1)
+
+/*let rasinkare = [64]
+const math.sqrt(x);
+console.log(rasinkare)
+
+
+let i;
+for (i = 0; < rasinkare.length > i; i++) { 
+console.log(rasinkare[i].length)
+}*/
+/*
+function rasinkare() {
+    let a = Math.sqrt(1);
+    let b = Math.sqrt(2);
+    let c = Math.sqrt(3);
+    let d = Math.sqrt(4);
+    let e = Math.sqrt(5);
+  
+    var x = a + b + c + d + e;
+    rasinkare.getElementById();
+}*/
+
